9f47b6c809b14257a4f18d2659f2d35391d9976b
Eugenio Fortanely <erf@mit.edu>
2013-05-27 11:30:01 -0400

    Added more metrics

--- mullToBazaarNoPrint.py
+++ mullToBazaarNoPrint.py
@@ -1,6 +1,6 @@
 import random
 
-attempts = 1000000
+attempts = 1000
 failures = 0
 count = 0
 x=[]
@@ -45,3 +45,11 @@ print "success_rate"
 print ((attempts - failures)*1.0)/attempts
 print "Average hand size kept"
 print (sum([7-i for i in x])*1.0)/attempts
+print ""
+for i in range(8):
+  print "Number of "+str(7-i)+" card hands kept"
+  num_of_size_i =  len([k for k in x if k==i])
+  print num_of_size_i
+  print "% chance of keeping hand of size "+str(7-i)
+  print (num_of_size_i*100.0)/attempts
+  print ""
